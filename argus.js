f56cadb6af2a6676c9d3c74400f9a9f8dce982ad
Sophia Li <sophia.li@aristotlemetadata.com>
2024-07-30 14:32:24 +1000

    changes requested by the review

--- argus.js
+++ argus.js
@@ -7,27 +7,27 @@ const ArgusJS = function(token, mdrUrl) {
 
     window.addEventListener("message", (event) => {
         if (event.data.argusMessageId === MESSAGE_ARGUS_TOKEN_REFRESH) {
-            currentToken = event.data.token;
+            currentToken = event.data.token
         }
     }, false);
 
     return {
         get: (url) => fetch(`${mdrUrl}${url}`, { "method": "GET", "headers": { "Authorization": "Bearer " + currentToken.access } }),
-        post: (url, data) => fetch(`${mdrUrl}${url}`, { "method": "POST", "headers": { "Authorization": "Bearer " + currentToken.access }, body: JSON.stringify(data) }),
-        put: (url, data) => fetch(`${mdrUrl}${url}`, { "method": "PUT", "headers": { "Authorization": "Bearer " + currentToken.access }, body: JSON.stringify(data) }),
-        patch: (url, data) => fetch(`${mdrUrl}${url}`, { "method": "PATCH", "headers": { "Authorization": "Bearer " + currentToken.access }, body: JSON.stringify(data) }),
+        post: (url, data) => fetch(`${mdrUrl}${url}`, { "method": "POST", "headers": { "Content-Type": "application/json; charset=UTF-8", "Authorization": "Bearer " + currentToken.access }, body: JSON.stringify(data) }),
+        put: (url, data) => fetch(`${mdrUrl}${url}`, { "method": "PUT", "headers": { "Content-Type": "application/json; charset=UTF-8", "Authorization": "Bearer " + currentToken.access }, body: JSON.stringify(data) }),
+        patch: (url, data) => fetch(`${mdrUrl}${url}`, { "method": "PATCH", "headers": { "Content-Type": "application/json; charset=UTF-8", "Authorization": "Bearer " + currentToken.access }, body: JSON.stringify(data) }),
         delete: (url) => fetch(`${mdrUrl}${url}`, { "method": "DELETE", "headers": { "Authorization": "Bearer " + currentToken.access } }),
         graphQL: (query) => fetch(mdrUrl + "/api/graphql/json", { "method": "POST", "headers": { "Accept": "application/json", "Content-Type": "application/graphql", "Authorization": "Bearer " + currentToken.access}, body: query})
     }
 }
 
-const initArgusJS = function() {
+export function initArgusJS() {
     return new Promise((resolve) => {
         const requestId = Date.now();
 
         const argusTokenResponseHandler = function(event) {
             if (event.data.argusMessageId === MESSAGE_ARGUS_TOKEN_RESPONSE && event.data.requestId === requestId) {
-                resolve(ArgusJS(event.data.token));
+                resolve(ArgusJS(event.data.token, event.data.mdr_url))
                 window.removeEventListener("message", argusTokenResponseHandler)
             }
         }
