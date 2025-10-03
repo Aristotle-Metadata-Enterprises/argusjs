c0ec40a1f9ed368d469d313a844c6466171ec17b
ryschneider <ryanjs929@gmail.com>
2025-10-03 13:17:28 +1000

    authentication with email/pass creds

--- dev/main.js
+++ dev/main.js
@@ -1,10 +1,43 @@
-const generateToken = async (mdrUrl, email, password) => {
-    
+// format url to http://x.x.x
+const formatUrl = (url) => {
+    if (!url.startsWith("http")) {
+        url = "http://" + url
+    }
+    url = url.replace(/\/$/, "") // remove trailing slash
+    return url
 }
 
-const refreshToken = async () => {
+const generateToken = async (mdrUrl, email, password) => {
     const response = await fetch(
-        mdr_url + "/api/jwt/token/refresh/",
+        formatUrl(mdrUrl) + "/api/jwt/token/",
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ email, password })
+        }
+    )
+
+    if (response.ok) {
+        const token = await response.json()
+        return token
+    } else {
+        let errorText
+        try {
+            const errorData = await response.json()
+            errorText = errorData.detail || JSON.stringify(errorData)
+        } catch {
+            errorText = await response.text()
+        }
+        console.error(`Request failed with code ${response.status} ${response.statusText}\n${errorText}`)
+        return null
+    }
+}
+
+const refreshToken = async (mdrUrl, token) => {
+    const response = await fetch(
+        formatUrl(mdrUrl) + "/api/jwt/token/refresh/",
         {
             method: "POST",
             headers: {
@@ -15,32 +48,43 @@ const refreshToken = async () => {
         }
     )
     const newToken = await response.json()
-    token.access = newToken.access
+    return newToken
 }
 
-const postMessageToApp = (data) => {
-    document.querySelector("#app").contentWindow.postMessage(data, app_url)
+const postMessageToApp = (appUrl, data) => {
+    document.querySelector("#app").contentWindow.postMessage(data, appUrl)
 }
 
-window.onload = () => {
+let argusToken = null
+
+window.onload = async () => {
+    console.log("testing")
     const urlParams = new URLSearchParams(window.location.search)
 
     const appUrl = urlParams.get("app_url")
     const mdrUrl = urlParams.get("mdr_url")
-    const token = urlParams.get("token")
+
+    argusToken = urlParams.get("token")
+    const email = urlParams.get("email")
+    const password = urlParams.get("password")
 
     // serve the /noapp forms, which can be used to populate query fields
-    if (!appUrl || !mdrUrl || !token) {
+    if (!appUrl || !mdrUrl || (!argusToken && (!email || !password))) {
         document.querySelector("#app").src = "/noapp"
         return
+    }
+
+    // generate token if not provided
+    if (!argusToken) {
+        argusToken = await generateToken(mdrUrl, email, password)
     }
 
     // refresh token every 3 mins
     window.setInterval(async () => {
         document.querySelector("#errorMessage").style.display = "none"
         try {
-            await refreshToken()
-            postMessageToApp({ argusMessageId: "argus-token-refresh", token: token })
+            argusToken = await refreshToken(mdrUrl, argusToken)
+            postMessageToApp(appUrl, { argusMessageId: "argus-token-refresh", token: argusToken })
         } catch (e) {
             document.querySelector("#errorMessage").style.display = "block"
             console.error("Could not connect to aristotle to reresh token", e)
@@ -50,11 +94,11 @@ window.onload = () => {
     // respond to messages with mdr url & token
     window.addEventListener("message", (event) => {
         if (event.data.argusMessageId === "argus-token-request") {
-            postMessageToApp({
+            postMessageToApp(appUrl, {
                 argusMessageId: "argus-token-response",
                 requestId: event.data.requestId,
-                token,
-                mdr_url
+                token: argusToken,
+                mdr_url: formatUrl(mdrUrl)
             })
         }
     }, false)
