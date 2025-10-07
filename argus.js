956dac28949001ff3fb74b1bcd8ee0b10e1f2eb1
ryschneider <ryanjs929@gmail.com>
2025-10-07 14:07:42 +1100

    allow authentication with api token

--- argus.js
+++ argus.js
@@ -3,21 +3,32 @@ const MESSAGE_ARGUS_TOKEN_REQUEST = "argus-token-request";
 const MESSAGE_ARGUS_TOKEN_RESPONSE = "argus-token-response";
 
 const ArgusJS = function(token, mdrUrl) {
-    let currentToken = token;
+    let auth
+    if (typeof token === "string" || token instanceof String) {
+        auth = "Token " + token // api token
+    } else {
+        auth = "Bearer " + token.access // access token
+    }
 
     window.addEventListener("message", (event) => {
         if (event.data.argusMessageId === MESSAGE_ARGUS_TOKEN_REFRESH) {
-            currentToken = event.data.token
+            const token = event.data.token
+
+            if (typeof token === "string" || token instanceof String) {
+                auth = "Token " + token
+            } else {
+                auth = "Bearer " + token.access
+            }
         }
     }, false);
 
     return {
-        get: (url) => fetch(`${mdrUrl}${url}`, { "method": "GET", "headers": { "Authorization": "Bearer " + currentToken.access } }),
-        post: (url, data) => fetch(`${mdrUrl}${url}`, { "method": "POST", "headers": { "Content-Type": "application/json; charset=UTF-8", "Authorization": "Bearer " + currentToken.access }, body: JSON.stringify(data) }),
-        put: (url, data) => fetch(`${mdrUrl}${url}`, { "method": "PUT", "headers": { "Content-Type": "application/json; charset=UTF-8", "Authorization": "Bearer " + currentToken.access }, body: JSON.stringify(data) }),
-        patch: (url, data) => fetch(`${mdrUrl}${url}`, { "method": "PATCH", "headers": { "Content-Type": "application/json; charset=UTF-8", "Authorization": "Bearer " + currentToken.access }, body: JSON.stringify(data) }),
-        delete: (url) => fetch(`${mdrUrl}${url}`, { "method": "DELETE", "headers": { "Authorization": "Bearer " + currentToken.access } }),
-        graphQL: (query) => fetch(mdrUrl + "/api/graphql/json", { "method": "POST", "headers": { "Accept": "application/json", "Content-Type": "application/graphql", "Authorization": "Bearer " + currentToken.access}, body: query})
+        get: (url) => fetch(`${mdrUrl}${url}`, { "method": "GET", "headers": { "Authorization": auth } }),
+        post: (url, data) => fetch(`${mdrUrl}${url}`, { "method": "POST", "headers": { "Content-Type": "application/json; charset=UTF-8", "Authorization": auth }, body: JSON.stringify(data) }),
+        put: (url, data) => fetch(`${mdrUrl}${url}`, { "method": "PUT", "headers": { "Content-Type": "application/json; charset=UTF-8", "Authorization": auth }, body: JSON.stringify(data) }),
+        patch: (url, data) => fetch(`${mdrUrl}${url}`, { "method": "PATCH", "headers": { "Content-Type": "application/json; charset=UTF-8", "Authorization": auth }, body: JSON.stringify(data) }),
+        delete: (url) => fetch(`${mdrUrl}${url}`, { "method": "DELETE", "headers": { "Authorization": auth } }),
+        graphQL: (query) => fetch(mdrUrl + "/api/graphql/json", { "method": "POST", "headers": { "Accept": "application/json", "Content-Type": "application/graphql", "Authorization": auth}, body: query})
     }
 }
 
@@ -35,4 +46,4 @@ export function initArgusJS() {
         window.addEventListener("message", argusTokenResponseHandler, false);
         top.postMessage({ argusMessageId: MESSAGE_ARGUS_TOKEN_REQUEST, requestId }, "*")
     })
-};
+}
--- dev/main.js
+++ dev/main.js
@@ -7,52 +7,6 @@ const formatUrl = (url) => {
     return url
 }
 
-// fetch jwt token from mdr
-const fetchToken = async (mdrUrl, email, password) => {
-    const response = await fetch(
-        formatUrl(mdrUrl) + "/api/jwt/token/",
-        {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({ email, password })
-        }
-    )
-
-    if (response.ok) {
-        const token = await response.json()
-        return token
-    } else {
-        let errorText
-        try {
-            const errorData = await response.json()
-            errorText = errorData.detail || JSON.stringify(errorData)
-        } catch {
-            errorText = await response.text()
-        }
-        console.error(`Request failed with code ${response.status} ${response.statusText}\n${errorText}`)
-        return null
-    }
-}
-
-// refresh jwt token
-const refreshToken = async (mdrUrl, token) => {
-    const response = await fetch(
-        formatUrl(mdrUrl) + "/api/jwt/token/refresh/",
-        {
-            method: "POST",
-            headers: {
-                "Authorization": "Bearer " + token.access,
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({ "refresh": token.refresh })
-        }
-    )
-    const newToken = await response.json()
-    return newToken
-}
-
 // fetch manifest from app
 const getManifest = async (appUrl) => {
     const response = await fetch(formatUrl(appUrl) + "/aristotle-manifest.json", { headers: { "Content-Type": "application/json" } })
@@ -73,32 +27,10 @@ const getManifest = async (appUrl) => {
     }
 }
 
-// add scope to token payload
-const addTokenScope = (token, scope) => {
-    // jwt token has format header.payload.signature, each encoded base64
-    const addJwtScope = (jwt) => {
-        const parts = jwt.split(".")
-        const payload = JSON.parse(atob(parts[1]))
-        payload["scope"] = scope
-        parts[1] = btoa(JSON.stringify(payload))
-        return parts.join(".")
-    }
-
-    // token is JSON object {access, refresh}, each jwt tokens
-    return { access: addJwtScope(token.access), refresh: addJwtScope(token.refresh) }
-}
-
-const generateToken = async (mdrUrl, email, password, manifest) => {
-    const token = await fetchToken(mdrUrl, email, password)
-    return addTokenScope(token, manifest.scope)
-}
-
 // post message to app's argus.js
 const postMessageToApp = (appUrl, data) => {
     document.querySelector("#app").contentWindow.postMessage(data, appUrl)
 }
-
-let currentToken = null
 
 window.onload = async () => {
     const urlParams = new URLSearchParams(window.location.search)
@@ -106,12 +38,10 @@ window.onload = async () => {
     const appUrl = urlParams.get("app_url")
     const mdrUrl = urlParams.get("mdr_url")
 
-    currentToken = urlParams.get("token")
-    const email = urlParams.get("email")
-    const password = urlParams.get("password")
+    const currentToken = urlParams.get("token")
 
     // serve the /noapp forms, which can be used to populate query fields
-    if (!appUrl || !mdrUrl || (!currentToken && (!email || !password))) {
+    if (!appUrl || !mdrUrl || !currentToken) {
         document.querySelector("#app").src = "/noapp"
         return
     }
@@ -121,25 +51,6 @@ window.onload = async () => {
         document.title = `${manifest.name} - Metadata Registry`
         return manifest
     })
-
-    // parse token
-    if (currentToken) {
-        currentToken = JSON.parse(currentToken)
-    } else { // generate token if not provided
-        currentToken = await generateToken(mdrUrl, email, password, await manifest)
-    }
-
-    // refresh token every 3 mins
-    window.setInterval(async () => {
-        document.querySelector("#errorMessage").style.display = "none"
-        try {
-            currentToken = await refreshToken(mdrUrl, currentToken)
-            postMessageToApp(appUrl, { argusMessageId: "argus-token-refresh", token: currentToken })
-        } catch (e) {
-            document.querySelector("#errorMessage").style.display = "block"
-            console.error("Could not connect to aristotle to reresh token", e)
-        }
-    }, 3 * 60 * 1000)
 
     // respond to messages with mdr url & token
     window.addEventListener("message", (event) => {
