ef0d18f42cb16703b83e997558620674f7518de5
ryschneider <ryanjs929@gmail.com>
2025-10-31 11:25:34 +1100

    request id test

--- tests/argus.test.js
+++ tests/argus.test.js
@@ -110,10 +110,58 @@ test("Test ArgusJS with incorrect API token", async () => {
 }, 1000)
 
 test("Test ArgusJS with incorrect mdr url", async () => {
-    const mdrUrl = "https://www.ametadataregistry.com"
+    const mdrUrl = "https://www.somemetadataregistry.com"
     const token = "0123456789abcdef0123456789abcdef01234567"
     const auth = `Token ${token}` // api token auth
 
     const argus = await setupArgus("https://not.my.url", token)
     await expect(testMethods(argus, mdrUrl, auth)).rejects.toThrowError()
+}, 1000)
+
+test("Test ArgusJS checks request ID", async () => {
+    const mdrUrl = "https://www.anothermetadataregistry.com"
+    const token = "0123456789abcdef0123456789abcdef01234567"
+    const auth = `Token ${token}` // api token auth
+
+    // initialise argus
+    const argusPromise = initArgusJS()
+
+    // check for message requesting a token
+    expect(window.top.postMessage).toHaveBeenCalled()
+    const [requestMsg, target] = window.top.postMessage.mock.calls[0]
+    expect(requestMsg.argusMessageId).toBe("argus-token-request")
+    expect(requestMsg.requestId).toBeDefined()
+    expect(target).toBeDefined()
+
+    // send response with wrong request id
+    const wrongResponseMsg = new MessageEvent("message", {
+        data: {
+            argusMessageId: "argus-token-response",
+            requestId: `${requestMsg.requestId}1`,
+            token: "notmytoken",
+            mdr_url: "https://not.my.url"
+        }
+    })
+    window.dispatchEvent(wrongResponseMsg)
+
+    // check argus not resolved after 100 ms
+    const timeout = new Promise(res => setTimeout(() => res("timeout"), 100))
+    expect(await Promise.race([argusPromise, timeout])).toBe("timeout")
+
+    // send response with correct request id
+    const responseMsg = new MessageEvent("message", {
+        data: {
+            argusMessageId: "argus-token-response",
+            requestId: requestMsg.requestId,
+            token: token,
+            mdr_url: mdrUrl
+        }
+    })
+    window.dispatchEvent(responseMsg)
+
+    const argus = await argusPromise
+    expect(argus).toBeDefined()
+
+    // check argus is set up correctly
+    await testMethods(argus, mdrUrl, auth)
 }, 1000)
